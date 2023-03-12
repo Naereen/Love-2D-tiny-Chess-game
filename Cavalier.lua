84d834d69cb057c6eeb94e0a12b2b4f1c5c301fc
Lilian Besson <lilian.besson@crans.org>
2023-03-13 00:01:29 +0100

    Squelette pour les autres pièces : cavalier, fou, reine, roi, tour.

---
+++ Cavalier.lua
@@ -0,0 +1,20 @@
+Cavalier = Class{__includes = Meeple}
+
+function Cavalier:init(i, j, est_blanc)
+    self.i = i
+    self.j = j
+    -- https://en.wikipedia.org/wiki/Chess_symbols_in_Unicode
+    self.est_blanc = est_blanc
+    if est_blanc then
+        self.emoji = '♘'
+    else
+        self.emoji = '♞'
+    end
+end
+
+function Cavalier:mouvement_legal(new_i, new_j)
+    if Meeple:mouvement_legal(new_i, new_j) == false then -- si on sort du plateau
+        return false -- mouvement illégal
+    end
+    return TODO:
+end
