77189eaf1fb6a3bd518de7507b3161459e6c3aca
Lilian Besson <lilian.besson@crans.org>
2023-03-23 20:38:44 +0100

    Mouvement des pions bien corrigés.

--- Pion.lua
+++ Pion.lua
@@ -18,19 +18,21 @@ function Pion:mouvement_legal(new_i, new_j, case_arrivee, plateau)
     if piece:mouvement_legal(new_i, new_j, case_arrivee, plateau) == false then -- si trivialement illégal
         return false -- mouvement illégal
     end
+    -- si == -1 ou == +1 il faut vérifier que la case d'arrivée est de la couleur adverse
+    if (self.i == new_i - 1 or self.i == new_i + 1) and case_arrivee.est_vide then
+        return false
+    end
     if self.est_blanc then
         -- cas spécial du premier coup du pion
         if self.j == 7 and new_i == self.i and new_j == 5 then
             return true
         end
         return (new_j == self.j - 1) and (self.i == new_i or self.i == new_i - 1 or self.i == new_i + 1)
-        -- FIXME: si == -1 ou == +1 il faut vérifier que la case d'arrivée est de la couleur adverse
     else
         -- cas spécial du premier coup du pion
         if self.j == 2 and new_i == self.i and new_j == 4 then
             return true
         end
         return (new_j == self.j + 1) and (self.i == new_i or self.i == new_i - 1 or self.i == new_i + 1)
-        -- FIXME: si == -1 ou == +1 il faut vérifier que la case d'arrivée est de la couleur adverse
     end
 end
--- README.md
+++ README.md
@@ -10,9 +10,10 @@ Un petit jeu d'échec écrit en Lua avec le framework Löve2D (https://love2d.or
 ![](demo_plateau_initial.png)
 
 - [x] Calcul des coups possibles pour chaque type de pièces ;
+- [x] Sélection de la pièce à jouer, et de la case de destination ;
 - [ ] Alternance joueur blanc, joueur noir, etc. (machine à état basique) ;
-- [ ] Sélection de la pièce à jouer, et de la case de destination ;
 - [ ] Jeu infini, jusqu'à ce qu'un Roi ait été pris ;
+- [ ] Connecter le joueur noir à une API en ligne de commande d'un bon solveur d'échec (par exemple [stockfish](https://stockfishchess.org/) ?).
 
 ## Ce que je ne ferai pas (pas envie)
 
